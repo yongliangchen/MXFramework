a47068ff991449bde19c52de9329a66afe410239
陈永亮 <451319296@qq.com>
2020-11-30 16:38:55 +0800

    add：添加了场景管理

---
+++ Assets/ImportPlugins/MXFramework5.0/Core/Scenes/LoadSceneData.cs
@@ -0,0 +1,8 @@
+﻿namespace Mx.Scene
+{
+    public class LoadSceneData
+    {
+        public static string NextSceneName { get; set; }
+        public static string LoadSceneUIName { get; set; }
+    }
+}
